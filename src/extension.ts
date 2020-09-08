058188e13452659885b33120ce8572102c5d128e
Xackery Xtal <xackery@gmail.com>
2020-09-07 21:38:27 -0700

    Refactored and simplified project

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,104 @@
-"use strict";
-import * as vscode from 'vscode';
-import EQEmuApiData from "./EQEmuApiData";
-import { EQEmuAutocomplete } from "./EQEmuAutocomplete";
-import { EQEmuHover } from "./EQEmuHover";
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
 
-const LUA_MODE = { language: "lua", scheme: "file" };
+import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let dataPath = context.asAbsolutePath("./data");
-    const eqEmuApiData = new EQEmuApiData(dataPath);
 
-    context.subscriptions.push(
-        vscode.languages.registerCompletionItemProvider(
-            LUA_MODE,
-            new EQEmuAutocomplete(eqEmuApiData),
-            '.'
-        )
-    );
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('lua', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			const linePrefix = document.lineAt(position).text.substr(0, position.character);
+			if (position.character !== 0 && !linePrefix.endsWith('')) {
+				return undefined;
+			}
 
-    context.subscriptions.push(
-        vscode.languages.registerHoverProvider(
-            LUA_MODE,
-            new EQEmuHover(eqEmuApiData)
-        )
-    );
-}
+			const results:vscode.CompletionItem[] = [];
+			
+			const eqCompletion = new vscode.CompletionItem('eq', vscode.CompletionItemKind.Class);
+			eqCompletion.insertText = new vscode.SnippetString("eq.");
+			eqCompletion.documentation = new vscode.MarkdownString("Everquest Global Namespace");
+			eqCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+			results.push(eqCompletion);
+			return results;
+		}
+	}));
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('lua', {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.endsWith('e.')) {
+					return undefined;
+				}
+
+				return [
+					new vscode.CompletionItem('self', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('other', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
+				];
+			}
+		}, '.' // triggered whenever a '.' is being typed
+	));
+
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('lua', {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.endsWith('e.other:')) {
+					return undefined;
+				}
+
+				const say = new vscode.CompletionItem('Say', vscode.CompletionItemKind.Function);
+				say.insertText = new vscode.SnippetString("Say(${1|message|})");
+				say.documentation = new vscode.MarkdownString("Say a message");
+				return [
+					say
+				];
+			}
+		}, ':' // triggered whenever a '.' is being typed
+	));
+
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('lua', {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.endsWith('eq.')) {
+					return undefined;
+				}
+				const results = [];
+
+				const debug = new vscode.CompletionItem('debug', vscode.CompletionItemKind.Method);
+				
+				debug.detail = "debug(message:string) -- void";
+				debug.insertText = new vscode.SnippetString('debug(${0:message:string})');
+				debug.documentation = new vscode.MarkdownString("Echos a GM only debug message");
+				results.push(debug);
+				return results;
+			}
+		}, '.' // triggered whenever a '.' is being typed
+	));
+
+	context.subscriptions.push(vscode.languages.registerHoverProvider('lua', {
+		provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {return new Promise<vscode.Hover>((resolve, reject) => {
+			const wordRange = document.getWordRangeAtPosition(position);
+            if (!wordRange) {
+                return reject();
+            }
+			const linePrefix = document.lineAt(position).text.substr(0, position.character);
+			const lineText = document.lineAt(position.line).text;
+            let lineTillCurrentWord = lineText.substr(0, wordRange.end.character);
+            if (lineTillCurrentWord.search(" ") > 0) {
+                lineTillCurrentWord = lineTillCurrentWord.split(" ").pop()!;                
+            }
+			
+			if (lineTillCurrentWord === "eq") {
+				return resolve(new vscode.Hover( new vscode.MarkdownString("```lua\neq\n```\n[eq on questapi](https://questapi.firebaseapp.com/lua/eq)\n\nEQ Namespace are globally accessible functions"), wordRange));
+			}
+
+			if (lineTillCurrentWord === "eq.debug") {
+				return resolve(new vscode.Hover(new vscode.MarkdownString("```lua\neq.debug(message:string) -- void\n```\n[eq.debug on questapi](https://questapi.firebaseapp.com/lua/eq/debug)\n\nEchos a GM only debug message"), wordRange));
+            }
+
+			return resolve(new vscode.Hover(new vscode.MarkdownString("unknown: "+lineTillCurrentWord), wordRange));
+            //return reject();
+		});}
+	}));
 }
